37e016d0c7a286c7bc9c74c701e04f95f3876fb8
itomayuuko <itomayuuko@192.168.11.4>
2020-07-10 14:09:33 +0900

    定期預金

--- software-last/src/Main.java
+++ software-last/src/Main.java
@@ -1,7 +1,6 @@
 
 import java.util.*;
 
-
 public class Main {
 
 	public static void main (String [] args) 
@@ -14,18 +13,33 @@ public class Main {
 		//MyAccount インスタンス化
 		MyAccount myAcc = new MyAccount();
 		
-		//以下、学籍番号G20194の例：
-
+		//ArrayList<MyAccount> myAcc = new ArrayList<MyAccount>();
+		
+		//System.out.println("作る口座の数を設定してください");
+		//int n = sc.nextInt();
+		
+		//以下、学籍番号G20194の例
 		//口座番号の設定
+		//for(int i=0;i>n;i++) {
 		System.out.println("口座番号を設定してください");
+		//int acc = sc.nextInt();
+		
 		myAcc.setAccNum(sc.nextInt());
 		
 		//暗証番号の設定
 		System.out.println("暗証番号を設定してください");
+		//int pass = sc.nextInt();
 		myAcc.setPass(sc.nextInt());
 		
+		
 		//残高の設定
+		//System.out.println("初期残高を設定してください");
+		//int bal = sc.nextInt();
 		myAcc.setBalance(291940);
+		
+		//MyAccount acc1 =new MyAccount(acc,pass,bal);
+		//myAcc.add(acc1);
+		//}
 		
 		//getterを用いた表示
 		System.out.println("Account Number: "+ myAcc.getAccNum()); //口座番号
@@ -46,15 +60,34 @@ public class Main {
 			System.out.println("2: 出金");
 			System.out.println("3: 残高照会");
 			System.out.println("4: 振り込み");
-			 System.out.println("5: 終了");
+			System.out.println("5: 終了");
 			
 			int option = sc.nextInt();
 			if (option == 1) {
 				System.out.println("入金額を入力してください");
 				int deposit = sc.nextInt();
-				System.out.println("Start deposit: "+ deposit + " JPY");
-				atm.deposit(deposit); //入金処理
-				atm.showCurrentBalance(); //表示
+				System.out.println("1:普通預金か2:定期預金か選択してください");
+				int dep = sc.nextInt();
+				if(dep == 1) {
+					System.out.println("Start deposit: "+ deposit + " JPY");
+					atm.deposit(deposit); //入金処理
+					atm.showCurrentBalance(); //表示
+				}
+				if(dep == 2) {
+					System.out.println("新しく口座を作ります");
+					MyAccount myAcc2 =new MyAccount();
+					System.out.println("口座番号を設定してください");
+					myAcc2.setAccNum(sc.nextInt());
+					System.out.println("暗証番号を設定してください");
+					myAcc2.setPass(sc.nextInt());
+					myAcc2.setBalance(0);
+					TestATM atm2 = new TestATM(myAcc2);
+					System.out.println("Start deposit: "+ deposit + " JPY");
+					atm2.deposit(deposit); //入金処理
+					atm2.showCurrentBalance(); //表示
+					atm2.afterdep();
+				}
+				
 			}
 			
 			//[2]-2
@@ -69,7 +102,12 @@ public class Main {
 			atm.showCurrentBalance(); //表示
 		}
 			else if (option == 4) {
-			//振り込み処理を書いてね
+				System.out.println("振込先の口座を指定してください");
+				int payee = sc.nextInt(); //振込先の口座番号
+				int money = sc.nextInt(); //振込額
+				System.out.println("Start withdraw: "+ money + " JPY");
+				atm.withdraw(money); //出金処理
+				atm.showCurrentBalance(); //表示
 		}
 			else if (option == 5) {
 			System.exit(0);
